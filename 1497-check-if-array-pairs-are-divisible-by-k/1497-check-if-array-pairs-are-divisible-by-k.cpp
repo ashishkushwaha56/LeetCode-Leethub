3a909713c42ff877c5ae1a7ec8aab3c3608a756e
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2022-10-17 21:11:57 +0530

    Time: 395 ms (33.96%), Space: 69.8 MB (47.58%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -5,9 +5,9 @@ public:
         for(int i = 0;i<arr.size();i++){
             mb[((arr[i]%k)+k)%k]++;
         }
-        for(auto &it:mb){
-            cout<<it.first<<" "<<it.second<<endl;
-        }
+        // for(auto &it:mb){
+        //     cout<<it.first<<" "<<it.second<<endl;
+        // }
         for(auto &it:mb){
             if(it.first == 0) {if(it.second&1) return false;}
             else if(mb.find(k-it.first)==mb.end()) return false;
