24d2279a6419ae88f77a144f9d65c18bbd1568ea
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2022-10-24 18:45:43 +0530

    Added solution - LeetHub

---
+++ Earthquake and the Paint Shop - GFG/earthquake-and-the-paint-shop.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+struct alphanumeric {
+    string name;
+    int count;
+};
+class Solution {
+  public:
+    vector<alphanumeric> sortedStrings(int &N, vector<string> &A) {
+        map<string,int> mb;
+        for(auto &it:A){
+            mb[it]++;
+        }
+        vector<alphanumeric> v;
+        for(auto &it:mb){
+            alphanumeric a;
+            a.name = it.first;
+            a.count = it.second;
+            v.push_back(a);
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        cin.ignore();
+        vector<string> v;
+        for (int i = 0; i < N; i++) {
+            string s;
+            getline(cin, s);
+            v.push_back(s);
+        }
+        Solution ob;
+        vector<alphanumeric> ans = ob.sortedStrings(N, v);
+        for (auto u : ans) cout << u.name << " " << u.count << "\n";
+    }
+}
+// } Driver Code Ends
