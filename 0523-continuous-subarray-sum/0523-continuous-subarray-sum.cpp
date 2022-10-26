0a05ecec6c2d0d9c39b4adffcdba819d2e4468a1
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2022-10-26 19:52:35 +0530

    Time: 587 ms (17.21%), Space: 113.9 MB (26.56%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        for(int i = 0;i<nums.size();i++){
+        for(int i = 0;i<nums.size(); i++){
             nums[i] = nums[i]%k;
-        }
-        if(nums.size()<2){
-            return false;
         }
         unordered_map<int,int> mb;
         mb[0] = -1;
-        mb[nums[0]] = 0;
-        int sum = nums[0];
-        int l = sum;
-        for(int i = 1;i<nums.size();i++){
-            // l = sum;
-            sum+=nums[i];
-            if(mb.find(sum%k)!=mb.end() and i-mb[sum%k]>=2){
-                return true;
-            }
-//             if(l+nums[i] == sum){
-                
-//             }
-            // else{
+        int sum = 0;
+        for(int i = 0;i<nums.size();i++){
+            sum = (sum+nums[i]);
+            if(mb.find(sum%k) == mb.end()){
                 mb[sum%k] = i;
-            // }
-            // for(auto &it:mb){
-            //     cout<<it.first<<" "<<it.second<<endl;
-            // }
-        }
-        for(int i = 0;i<nums.size()-1;i++){
-            if(nums[i] == 0 and nums[i+1] == 0){
-                return true;
+            }
+            else{
+                if(i-mb[sum%k]>=2){
+                    return true;
+                }
             }
         }
-        return ((sum%k)?false:true);
+        return (sum%k == 0 and sum!=0)?true:false;
     }
 };
