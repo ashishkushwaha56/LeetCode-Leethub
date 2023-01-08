5476ff03ab943415b89c08ddd4f48e3a24ff420e
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-01-08 13:46:10 +0530

    Added solution - LeetHub

---
+++ Absolute difference divisible by K - GFG/absolute-difference-divisible-by-k.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long countPairs(int n, int arr[], int k) {
+        long long ans = 0;
+        unordered_map<int,int> mb;
+        for(int i = 0;i<n;i++){
+            mb[arr[i]%k]++;
+        }
+        for(auto &it:mb){
+            ans+=(long long)(it.second*(it.second-1))/2;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        
+        cin>>k;
+
+        Solution ob;
+        cout << ob.countPairs(n,arr,k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
