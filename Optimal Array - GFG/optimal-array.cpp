57e6d6812f58fe49a315a16e17c90aa1edd9a291
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-02-28 09:28:47 +0530

    Added solution - LeetHub

---
+++ Optimal Array - GFG/optimal-array.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> optimalArray(int n,vector<int> &v){
+        vector<int> ans;
+        int j = 0;
+        int cnt = 0;
+        for(int i = 0;i<n;i++, j+=(!(i&1))){
+            cnt+=abs(v[i]-v[j]);
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        Solution obj;
+        vector<int> ans=obj.optimalArray(n,a);
+        for(auto ele:ans){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
