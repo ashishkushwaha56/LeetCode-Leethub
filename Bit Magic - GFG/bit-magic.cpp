ae659048c85ee3b1be7ea32f3c97d99ca8f73033
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-05-13 10:52:32 +0530

    Added solution - LeetHub

---
+++ Bit Magic - GFG/bit-magic.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int bitMagic(int n, vector<int> &arr) {
+        // code here
+        unordered_map<string,int> mb;
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            if(arr[l]!=arr[r]){
+                mb[to_string((arr[l]+'0')+(arr[r]+'0'))]++;
+            }
+            l++;
+            r--;
+        }
+        int ans = 0;
+        for(auto &it:mb){
+            ans+=(it.second/2);
+            ans+=(it.second%2);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        int res = obj.bitMagic(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
