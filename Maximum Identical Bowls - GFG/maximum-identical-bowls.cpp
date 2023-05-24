18ed33075735e625f087e8ff7dcc5f504fccbd4a
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-05-24 23:41:24 +0530

    Added solution - LeetHub

---
+++ Maximum Identical Bowls - GFG/maximum-identical-bowls.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMaximum(int N, vector<int> &arr) {
+        if(N==1) return 1;
+        long long d = accumulate(arr.begin(),arr.end(),0ll);
+        // cout<<d<<endl;
+        int ans = 1;
+        // cout<<sqrt(d)<<endl;
+        for(int i =N;i>=2;i--){
+            if(d%i == 0){
+                ans = max(ans,i);
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> arr(N);
+        Array::input(arr,N);
+        
+        Solution obj;
+        int res = obj.getMaximum(N, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
