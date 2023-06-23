b6c4978a10c05a7eeb783e5def2c70fddf7587c5
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-06-23 22:51:16 +0530

    Time: 3 ms (62.06%), Space: 9.8 MB (20%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int helper(map<long long,int>&dp,long long n){
+        if(n == 1) return 0;
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
+        int ans;
+        if(n&1){
+            int a = helper(dp,n-1);
+            int b = helper(dp,n+1);
+            ans = 1+min(a,b);
+        }
+        else{
+            ans = 1+helper(dp,n/2);
+        }
+        return dp[n]=ans;
+    }
+    int integerReplacement(int n) {
+        map<long long,int> dp;
+        return helper(dp,n);
+    }
+};
