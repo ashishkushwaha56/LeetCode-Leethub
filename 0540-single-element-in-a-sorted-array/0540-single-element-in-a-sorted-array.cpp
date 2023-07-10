03b16f9912def6f0a4e72f539565ef189f7b0575
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-07-10 13:08:36 +0530

    Time: 24 ms (91.15%), Space: 22.2 MB (99.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0;i<nums.size();i++){
-            ans^=nums[i];
+        if(nums.size() ==1) return nums[0];
+        int l = 1;
+        int r = nums.size()-2;
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
+        
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            // cout<<mid<<endl;
+            if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]) return nums[mid];
+            else if(nums[mid]==nums[mid-1] and nums[mid]<nums[mid+1] and (mid+1)%2==0){
+                l=mid+1;
+            }
+            else if(nums[mid]==nums[mid+1] and nums[mid]>nums[mid-1] and (mid+1)%2!=0){
+                l = mid+1;
+            }
+            else if(nums[mid]==nums[mid-1] and nums[mid]<nums[mid+1] and (mid+1)%2!=0){
+                r = mid-1;
+            }
+            else if(nums[mid]==nums[mid+1] and nums[mid]>nums[mid-1] and (mid+1)%2==0){
+                r = mid-1;
+            }
         }
-        return ans;
+        return 0;
+        
     }
 };
