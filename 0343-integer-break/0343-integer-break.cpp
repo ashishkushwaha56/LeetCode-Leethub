289f2547bb4e30be71a5a148d283f4143b2a03f6
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2023-10-06 22:52:03 +0530

    Time: 3 ms (30.5%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int helper(vector<int>&dp,int n){
-        if(n == 0){
-            return 1;
-        }
-        if(n<0){
-            return 0;
-        }
-        if(dp[n]!=-1) return dp[n];
-        int ans = 0;
-        for(int i = 1;i<=n;i++){
-            ans = max(ans,i*helper(dp,n-i));
-        }
-        return dp[n] = ans;
-    }
     int integerBreak(int n) {
-        if(n == 2) return 1;
-        else if(n == 3) return 2;
         vector<int> dp(n+1,-1);
-        return helper(dp,n);
+        function<int(int,int)> helper = [&](int n,int k)->int{
+            if(n == 0) {
+                if(k<2) return 0;
+                else{
+                    return 1;
+                }
+            }
+            if(dp[n]!=-1) return dp[n];
+            int ans = 0;
+            for(int i = 1;i<=n;i++){
+                ans= max(ans,i*helper(n-i,k+1));
+            }
+            return dp[n] = ans;
+        };
+        return helper(n,0);
     }
 };
